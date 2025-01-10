291080b5fbf385c8a73bb4e712e12bd6d3fcc811
SATA <SATA@Windchill.com>
2025-01-10 22:42:02 +0530

    update

--- Project_Windchill/src/ext/training/utility/genericUtility/ReadAttributeExample.java
+++ Project_Windchill/src/ext/training/utility/genericUtility/ReadAttributeExample.java
@@ -1,71 +1,83 @@
 package ext.training.utility.genericUtility;
 
+import java.beans.PropertyVetoException;
+import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
-import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ptc.core.lwc.server.PersistableAdapter;
 import com.ptc.core.meta.common.UpdateOperationIdentifier;
 
+import wt.doc.WTDocument;
+import wt.doc.WTDocumentMaster;
 import wt.fc.PersistenceHelper;
 import wt.fc.QueryResult;
 import wt.method.RemoteAccess;
 import wt.method.RemoteMethodServer;
-import wt.part.WTPart;
 import wt.pds.StatementSpec;
 import wt.query.QuerySpec;
 import wt.query.SearchCondition;
 import wt.util.WTException;
+import wt.vc.config.ConfigHelper;
+import wt.vc.config.LatestConfigSpec;
 
-public class ReadAttributeExample implements RemoteAccess, Serializable {
-    
-    private static final long serialVersionUID = 1L;
+public class ReadAttributeExample implements RemoteAccess, Serializable{
+	
+	private static final long serialVersionUID=1L;
 
-    public static void main(String[] args) throws WTException, RemoteException, InvocationTargetException {
-        if (!RemoteMethodServer.ServerFlag) {
-            RemoteMethodServer.getDefault().invoke("readAttribute", ReadAttributeExample.class.getName(), null, new Class[0], new Object[0]);
-        } else {
-            readAttribute();
-        }
-    }
-
-    public static void readAttribute() throws WTException {
-        // Replace with the actual part number and attribute name
-        String partNumber = "0000000006";
-        String attributeName = "HelpAttribute";
-
-        // Find the WTPart
-        WTPart part = findWTPartByNumber(partNumber);
-
-        if (part == null) {
-            System.out.println("WTPart not found for number: " + partNumber);
-            return;
-        }
-
-        // Use PersistableAdapter to load the attribute
-        PersistableAdapter adapter = new PersistableAdapter(part, null, null, new UpdateOperationIdentifier());
-        adapter.load(attributeName);
-
-        // Retrieve the attribute value
-        String attributeValue = (String) adapter.get(attributeName);
-
-        if (attributeValue != null) {
-            System.out.println("Value of " + attributeName + " is: " + attributeValue);
-        } else {
-            System.out.println("Attribute " + attributeName + " is not set.");
-        }
-    }
-
-    private static WTPart findWTPartByNumber(String number) throws WTException {
-        // Use QuerySpec to find WTPart by its number
-        QuerySpec querySpec = new QuerySpec(WTPart.class);
-        querySpec.appendWhere(new SearchCondition(WTPart.class, WTPart.NUMBER, SearchCondition.EQUAL, number), null);
-
-        QueryResult queryResult = PersistenceHelper.manager.find((StatementSpec) querySpec);
-
-        if (queryResult.hasMoreElements()) {
-            return (WTPart) queryResult.nextElement();
-        }
-        return null;
-    }
+public static void main(String[] args) throws Exception{
+	
+	RemoteMethodServer remoteMethodServer = RemoteMethodServer.getDefault();
+	remoteMethodServer.setUserName("wcadmin");
+	remoteMethodServer.setPassword("wcadmin");
+	
+	run();
 }
+
+	public static void run( ) throws WTException,PropertyVetoException, IOException {
+		final String color = "color";
+		final String address = "address";
+
+		List<String> attributeList =new ArrayList<String>();
+		attributeList.add(color);
+		attributeList.add(address);
+
+		QuerySpec qs = new QuerySpec(WTDocument.class);
+		//WTDocument  doc = null;
+		//boolean islatest=true;
+		System.out.println("Into Retriving IBA  value ");
+		try{
+			qs.appendWhere(new SearchCondition(WTDocument.class,WTDocument.NUMBER, SearchCondition.EQUAL,"0000000037"), null);
+			QueryResult qr = PersistenceHelper.manager.find((StatementSpec)qs);
+			System.out.println("Query size : "+qr.size());
+			if(qr.hasMoreElements()) {
+				WTDocument doc = (WTDocument) qr.nextElement();
+				WTDocumentMaster docMaster = (WTDocumentMaster) doc.getMaster();
+				QueryResult latestResult = ConfigHelper.service.filteredIterationsOf(docMaster, new LatestConfigSpec());
+				WTDocument latestObject = (WTDocument) latestResult.nextElement();
+						try{
+							PersistableAdapter adapter = new PersistableAdapter(latestObject, null,null, new UpdateOperationIdentifier());			
+							adapter.persist();
+							adapter.load(attributeList);
+							for (String attribute : attributeList) {
+				                Object value = adapter.get(attribute);
+				                System.out.println("Attribute: " + attribute + ", Value: " + value);
+				            
+								System.out.println(" last line in for loop ");
+							} 
+							System.out.println(" Out of For loop ");
+							
+						}catch(WTException e) {
+							e.printStackTrace();
+							System.out.println("Line 90 Inside catch block");
+						}
+					}
+		} catch(NumberFormatException nfe)
+		{
+			System.out.println("Into Catch Block");
+		}
+	}
+
+}
+
